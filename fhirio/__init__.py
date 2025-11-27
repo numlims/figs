a19764be1e186bb4cf57fe4d3573f7e57057ad8c
numlims <max.haubold@med.uni-greifswald.de>
2025-11-27 15:26:32 +0100

    specimen and patient

--- fhirio/__init__.py
+++ fhirio/__init__.py
@@ -1,13 +1,7 @@
 # automatically generated, DON'T EDIT. please edit init.ct from where this file stems.
 from dip import dig, dis
-class fhirhelp:
-    @staticmethod
-    def category(resource):
-        """
-         category returns the type from resource, 'MASTER', or 'ALIQUOTGROUP' or 'DERIVED' (this corresponds to dtype in the db). (was type).
-        """
-        ext = fhirhelp.extension(resource, "https://fhir.centraxx.de/extension/sampleCategory")
-        return dig(ext, "valueCoding/code")
+
+class fhirio:
     @staticmethod
     def extension(resource, url:str):
         """
@@ -20,13 +14,73 @@ class fhirhelp:
             if dig(ext, "url") == url:
                 return ext
     @staticmethod
+    def full_url(resource):
+        """
+         full_url return the full url of a fhir resource.
+        """
+        return dig(resource, "fullUrl")
+    @staticmethod
+    def identifiers(resource):
+        """
+         identifiers returns the identifiers of a resource as dict keyed by the identifier codes, eg: { "SAMPLEID": "abc", "EXTSAMPLEID": "cde" }
+        """
+        out = {}
+        if "identifier" not in resource:
+            return None
+        for identifier in dig(resource, "identifier"):
+            codings = dig(identifier, "type/coding")
+            if codings == None:
+                continue
+            for coding in codings:
+                if dig(coding, "system") == "urn:centraxx":
+                    # take the code as key. the value is a field of identifier.
+                    key = dig(coding, "code")
+                    out[key] = dig(identifier, "value")
+        return out
+    @staticmethod
+    def resource(entry):
+        """
+         resource returns the resource for an entry.
+         
+         all other methods take resource as argument.
+        """
+        return dig(entry, "resource")
+    @staticmethod
+    def resource_type(resource):
+        """
+         resource_type returns the resource type, Specimen or Observation. (was resourceType).
+        """
+        return dig(resource, "resourceType")
+    @staticmethod
+    def update_with_overwrite(resource) -> bool:
+        """
+         update_with_overwrite returns update with overwrite of a resource.
+        """
+        ext = fhirio.extension(resource, "https://fhir.centraxx.de/extension/updateWithOverwrite")
+        return dig(ext, "valueBoolean")
+class specimen(fhirio):
+    @staticmethod
+    def category(resource):
+        """
+         category returns the type from resource, 'MASTER', or 'ALIQUOTGROUP' or 'DERIVED' (this corresponds to dtype in the db). (was type).
+        """
+        ext = fhirio.extension(resource, "https://fhir.centraxx.de/extension/sampleCategory")
+        return dig(ext, "valueCoding/code")
+    @staticmethod
     def locationpath(resource):
         """
          locationpath gets the sampleLocation. (was lagerort).
         """
-        exta = fhirhelp.extension(resource, "https://fhir.centraxx.de/extension/sample/sampleLocation")
-        extb = fhirhelp.extension(exta, "https://fhir.centraxx.de/extension/sample/sampleLocationPath")
+        exta = fhirio.extension(resource, "https://fhir.centraxx.de/extension/sample/sampleLocation")
+        extb = fhirio.extension(exta, "https://fhir.centraxx.de/extension/sample/sampleLocationPath")
         return dig(extb, "valueString")
+    @staticmethod
+    def orga(resource):
+        """
+         orga returns the organisation or None if no organisation.
+        """
+        ext = fhirio.extension(resource, "https://fhir.centraxx.de/extension/sample/organizationUnit")
+        return dig(ext, "valueReference/identifier/value")
     @staticmethod
     def parent_fhirid(resource):
         """
@@ -57,13 +111,6 @@ class fhirhelp:
             if dig(coding, "code") == code:
                 return dig(resource, "subject/identifier/value")
     @staticmethod
-    def orga(resource):
-        """
-         orga returns the organisation or None if no organisation.
-        """
-        ext = fhirhelp.extension(resource, "https://fhir.centraxx.de/extension/sample/organizationUnit")
-        return dig(ext, "valueReference/identifier/value")
-    @staticmethod
     def restamount(resource):
         """
          restamount returns the restmenge of the sample. (was restmenge)
@@ -78,42 +125,23 @@ class fhirhelp:
         """
          sampleid returns the sample id of a resource.
         """
-        return dig(fhirhelp.identifiers(resource), code)
+        return dig(fhirio.identifiers(resource), code)
     @staticmethod
     def type(resource):
         """
          type returns the material type of the resource. (was material).
         """
         return dig(resource, "type/coding/0/code")
+class patient(fhirio):
     @staticmethod
-    def update_with_overwrite(resource) -> bool:
+    def patientid(resource, code:str="LIMSPSN"):
         """
-         update_with_overwrite returns update with overwrite of a resource.
+         patientid returns the patientid by code.
         """
-        ext = fhirhelp.extension(resource, "https://fhir.centraxx.de/extension/updateWithOverwrite")
-        return dig(ext, "valueBoolean")
+        return dig(fhirio.identifiers(resource), code)
     @staticmethod
-    def identifiers(resource):
+    def orga(resource):
         """
-         identifiers returns the identifiers of a resource as dict keyed by the identifier codes, eg: { "SAMPLEID": "abc", "EXTSAMPLEID": "cde" }
+         orga returns the organization unit for a patient resource.
         """
-        out = {}
-        if "identifier" not in resource:
-            return None
-        for identifier in dig(resource, "identifier"):
-            codings = dig(identifier, "type/coding")
-            if codings == None:
-                continue
-            for coding in codings:
-                if dig(coding, "system") == "urn:centraxx":
-                    # take the code as key. the value is a field of identifier.
-                    key = dig(coding, "code")
-                    out[key] = dig(identifier, "value")
-        return out
-    @staticmethod
-    def resourceType(resource):
-        """
-         resourceType returns the resource type, Specimen or Observation.
-        """
-        return dig(resource, "resourceType")
-
+        return dig(resource, "generalPractitioner/0/identifier/value")
--- fhirio/__main__.py
+++ fhirio/__main__.py
@@ -1,7 +1,10 @@
 # automatically generated, DON'T EDIT. please edit main.ct from where this file stems.
-    import fhirio as fio
-    import json
-    import argparse
+from fhirio import fhirio as fio, specimen as fios, patient as fiop
+import json
+import argparse
+import sys
+from dip import dig, dis
+import csv
 def main():
     """
      main holds a cli for fhirio. it takes a fhir file, resource index and
@@ -9,14 +12,15 @@ def main():
     """
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("file", help="fhir json file")
     parser.add_argument("index", help="index of resource, * for all")
     parser.add_argument("what", help="sampleid|patientid|locationpath|...")
+    parser.add_argument("file", help="fhir json file")    
     parser.add_argument("--csv", help="write output to named csv file")
+    parser.add_argument("-e", help="input file encoding")
     args = parser.parse_args()
     what = args.what.split("|")
     jsonin = None
-    with open(os.path.join(dir, file), "r", encoding=encoding) as f:    
+    with open(args.file, "r", encoding=args.e) as f:    
         jsonin = json.load(f)
     entries = dig(jsonin, "entry")
     if args.index == "*":
@@ -27,36 +31,62 @@ def main():
     for entry in searchentries:
         res = fio.resource(entry)
         row = {}
-        if "category" in what:
-            row["category"] = fio.category(res)
-        if "parent_fhirid" in what:
-            row["parent_fhirid"] = fio.parent_fhirid(res)
-        if "parent_sampleid" in what:
-            row["parent_sampleid"] = fio.parent_sampleid(res)
-        if "patientid" in what:
-            row["patientid"] = fio.patientid(res, "LIMSPSN")
-        if "orga" in what:
-            row["orga"] = fio.orga(res)
-        if "restamount" in what:
-            row["restamount"] = fio.restamount(res)
-        if "locationpath" in what:
-            row["locationpath"] = fio.locationpath(res)
-        if "sampleid" in what:
-            row["sampleid"] = fio.sampleid(res, "SAMPLEID")
-        if "resource_type" in what:
-            row["resource_type"] = fio.resource_type(res)
-        if "type" in what:
-            row["type"] = fio.type(res)
-        if "update_with_overwrite" in what:
-            row["update_with_overwrite"] = fio.update_with_overwrite(res)
+        whatresource = fios.resource_type(res)
+        if whatresource == "Patient":
+            row = fillpatient(res, what)
+        elif whatresource == "Specimen":
+            row = fillspecimen(res, what)
+        out.append(row)
     if args.csv is not None:
         with open(args.csv, "w") as f:
             writer = csv.writer(f)
             writer.writerow(list(out[0].keys()))
             for d in out:
                 writer.writerow(list(d.values()))
+            print(args.csv)
     else:
-        print(json.dumps(out, default=str))
-
+        print(json.dumps(out, default=str, indent=4))
+def fillpatient(resource, what):
+    """
+     fillpatient fills patient dict from resource.
+    """
+    row = {}
+    if "full_url" in what or "*" in what:
+        row["full_url"] = fiop.full_url(resource)
+    if "update_with_overwrite" in what or "*" in what:
+        row["update_with_overwrite"] = fiop.update_with_overwrite(resource)
+    if "orga" in what or "*" in what:
+        row["orga"] = fiop.orga(resource)
+    if "patientid" in what or "*" in what:
+        row["patientid"] = fiop.patientid(resource, code="LIMSPSN")
+    return row
+def fillspecimen(resource, what):
+    """
+     fillspecimen fills specimen dict from fhir resource.
+    """
+    row = {}
+    if "category" in what or "*" in what:
+        row["category"] = fios.category(resource)
+    if "parent_fhirid" in what or "*" in what:
+        row["parent_fhirid"] = fios.parent_fhirid(resource)
+    if "parent_sampleid" in what or "*" in what:
+        row["parent_sampleid"] = fios.parent_sampleid(resource)
+    if "patientid" in what or "*" in what:
+        row["patientid"] = fios.patientid(resource, "LIMSPSN")
+    if "orga" in what or "*" in what:
+        row["orga"] = fios.orga(resource)
+    if "restamount" in what or "*" in what:
+        row["restamount"] = fios.restamount(resource)
+    if "locationpath" in what or "*" in what:
+        row["locationpath"] = fios.locationpath(resource)
+    if "sampleid" in what or "*" in what:
+        row["sampleid"] = fios.sampleid(resource, "SAMPLEID")
+    if "resource_type" in what or "*" in what:
+        row["resource_type"] = fios.resource_type(resource)
+    if "type" in what or "*" in what:
+        row["type"] = fios.type(resource)
+    if "update_with_overwrite" in what or "*" in what:
+        row["update_with_overwrite"] = fios.update_with_overwrite(resource)
+    return row
 
 sys.exit(main())
